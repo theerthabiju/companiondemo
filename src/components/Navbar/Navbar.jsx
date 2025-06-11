f2fddc598688518d1a78b077d3c0ff7ef9dfcc7c
theerthabiju <theerthabiju2001@gmail.com>
2025-06-11 16:26:35 +0530

    navbar toggle updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,12 @@ import logo from "../../assets/images/logo.png";
 const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const hideLoginBtn = location.pathname === "/signin" || location.pathname === "/signup";
+  const hideLoginBtn =
+    location.pathname === "/signin" || location.pathname === "/signup";
 
   const [showDropdown, setShowDropdown] = useState(false);
   const dropdownRef = useRef(null);
+  const mobileMenuRef = useRef(null);
 
   const toggleDropdown = () => {
     setShowDropdown((prev) => !prev);
@@ -18,6 +20,13 @@ const Navbar = () => {
   const handleClickOutside = (e) => {
     if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
       setShowDropdown(false);
+    }
+  };
+
+  const closeMobileMenu = () => {
+    const menu = mobileMenuRef.current;
+    if (menu && menu.classList.contains("show")) {
+      menu.classList.remove("show");
     }
   };
 
@@ -30,7 +39,11 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg fixed-top">
         <div className="container">
-          <Link className="navbar-brand d-flex align-items-center" to="/">
+          <Link
+            className="navbar-brand d-flex align-items-center"
+            to="/"
+            onClick={closeMobileMenu}
+          >
             <img src={logo} alt="Companion" className="logo-img" />
             <span className="brand-name ms-2">Companion</span>
           </Link>
@@ -47,20 +60,42 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse mobile-menu" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+          <div
+            className="collapse navbar-collapse mobile-menu"
+            id="navbarSupportedContent"
+            ref={mobileMenuRef}
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/">Home</Link>
+                <Link
+                  className="nav-link text-black "
+                  to="/"
+                  onClick={closeMobileMenu}
+                >
+                  Home
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/companion/exploreride">Explore Rides</Link>
+                <Link
+                  className="nav-link text-black"
+                  to="/companion/exploreride"
+                  onClick={closeMobileMenu}
+                >
+                  Explore Rides
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/companion/postnewride">Post a New Ride</Link>
+                <Link
+                  className="nav-link text-black"
+                  to="/companion/postnewride"
+                  onClick={closeMobileMenu}
+                >
+                  Post a New Ride
+                </Link>
               </li>
 
               {/* Community with dropdown */}
-              <li className="nav-item position-relative">
+              <li className="nav-item " id="communityid">
                 <button
                   type="button"
                   className="nav-link text-black bg-transparent border-0"
@@ -73,35 +108,66 @@ const Navbar = () => {
               </li>
 
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/companion/Blogs">Stories</Link>
+                <Link
+                  className="nav-link text-black"
+                  to="/companion/Blogs"
+                  onClick={closeMobileMenu}
+                >
+                  Stories
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/companion/aboutus">About Us</Link>
+                <Link
+                  className="nav-link text-black"
+                  to="/companion/aboutus"
+                  onClick={closeMobileMenu}
+                >
+                  About 
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-black" to="/companion/Contact">Contact Us</Link>
+                <Link
+                  className="nav-link text-black"
+                  to="/companion/Contact"
+                  onClick={closeMobileMenu}
+                >
+                  Contact 
+                </Link>
               </li>
-            </ul>
-
+            </ul>{" "}
             <div className="d-flex justify-content-center gap-2 mb-2 mb-lg-0">
-              <button className="btn btn-outline profileicon">
+              <button
+                className="btn btn-outline profileicon"
+                onClick={closeMobileMenu}
+              >
                 <Link to="/Profile">
                   <i className="bi bi-person-circle profileicon"></i>
                 </Link>
               </button>
-
-              {!hideLoginBtn && (
-                <Link to="/signin" className="btn loginbtn text-black">
+           <button className="loginhomebtn" >
+  {!hideLoginBtn && (
+                <Link
+                  to="/signin"
+                  className="btn loginbtn text-black"
+                  onClick={closeMobileMenu}
+                >
                   Login
                 </Link>
               )}
+           </button>
+            
 
-              <button className="btn findaridebtn text-white" type="button">
+              <button
+                className="btn findaridebtn text-white"
+                type="button"
+                onClick={closeMobileMenu}
+              >
                 <Link className="nav-link" to="/companion/exploreride">
                   Find A Ride
                 </Link>
               </button>
             </div>
+        
           </div>
         </div>
       </nav>
@@ -111,12 +177,26 @@ const Navbar = () => {
         <div className="custom-dropdown-panel" ref={dropdownRef}>
           <ul className="list-unstyled m-0 text-center">
             <li>
-              <Link className="dropdown-link" to="/companion/community">
+              <Link
+                className="dropdown-link"
+                to="/companion/community"
+                onClick={() => {
+                  closeMobileMenu();
+                  setShowDropdown(false);
+                }}
+              >
                 Community List
               </Link>
             </li>
             <li>
-              <Link className="dropdown-link" to="/companion/add-community">
+              <Link
+                className="dropdown-link"
+                to="/companion/add-community"
+                onClick={() => {
+                  closeMobileMenu();
+                  setShowDropdown(false);
+                }}
+              >
                 Add Community
               </Link>
             </li>
